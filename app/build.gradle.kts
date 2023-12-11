76c199f34592c73d61afa29ec2ba58066ad16450
tommyprHub <tomasprimo.alu@iespacomolla.es>
2023-12-11 17:23:52 +0100

    avance en casa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Client
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    //Gson
+    implementation ("com.google.code.gson:gson:2.8.9")
+    //Image loading
+    implementation ("io.coil-kt:coil:1.4.0")
 }
